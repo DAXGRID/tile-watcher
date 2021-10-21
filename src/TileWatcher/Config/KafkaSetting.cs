a86509d547510f41f6c73c1a3c69406f33095d0e
Rune Nielsen <runenielsen@runbox.com>
2021-10-21 16:23:12 +0200

    adds default setup for host (#1)

---
+++ src/TileWatcher/Config/KafkaSetting.cs
@@ -0,0 +1,9 @@
+namespace TileWatcher
+{
+    public record KafkaSetting
+    {
+        public string? Consumer { get; set; }
+        public string? Server { get; set; }
+        public string? Topic { get; set; }
+    }
+}
